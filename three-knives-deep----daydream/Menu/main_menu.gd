93d550573dd508d064022e54f0fa39d4143458d3
PGarg111 <priyanshigarg111@icloud.com>
2025-09-27 19:48:24 -0400

    Edited GDScript
    Changed file destination for start button pressed

--- three-knives-deep----daydream/Menu/main_menu.gd
+++ three-knives-deep----daydream/Menu/main_menu.gd
@@ -3,13 +3,13 @@ extends Control
 
 
 
-func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://control.tscn")
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://Menu/control.tscn")
 
 
-func _on_settings_pressed() -> void:
+func _on_settings_pressed():
 	print("Settings pressed")
 
 
-func _on_exit_pressed() -> void:
+func _on_exit_pressed():
 	get_tree().quit()
