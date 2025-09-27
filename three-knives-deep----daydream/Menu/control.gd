a60c77969f46c3d32f3c9f83daab9c4f163bf2df
PGarg111 <priyanshigarg111@icloud.com>
2025-09-27 19:53:02 -0400

    Update control.gd
    Changed file destination

--- three-knives-deep----daydream/Menu/control.gd
+++ three-knives-deep----daydream/Menu/control.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_start_2_pressed():
-	get_tree().change_scene_to_file("res://main_menu.tscn")
+	get_tree().change_scene_to_file("res://Level 1/main_menu.tscn")
 
 
 func _on_exit_3_pressed():
