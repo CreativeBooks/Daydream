5482c9bed747af1d25ee72b0eec71748df552f09
AeroBotX <p.garg1606@gmail.com>
2025-10-03 17:49:51 -0400

    added script to win and loose

--- three-knives-deep----daydream/Game Over/you_win.gd
+++ three-knives-deep----daydream/Game Over/you_win.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-
 func _on_button_2_pressed() -> void:
 	get_tree().quit()
 
@@ -8,5 +7,3 @@ func _on_button_2_pressed() -> void:
 func _on_button_pressed() -> void:
 	GameState.reset_game()
 	get_tree().change_scene_to_file("res://Level 1/main_menu.tscn") 
-	
-	
