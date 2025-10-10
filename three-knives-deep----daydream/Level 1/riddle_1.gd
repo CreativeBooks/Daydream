7e3e5bc2afee4d5eb673d138d4f3f2e47c3f0369
AeroBotX <p.garg1606@gmail.com>
2025-10-10 17:45:27 -0400

    edited

--- three-knives-deep----daydream/Level 1/riddle_1.gd
+++ three-knives-deep----daydream/Level 1/riddle_1.gd
@@ -5,6 +5,14 @@ extends Node
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://Level 1/Riddle_1.tscn")
 	
+@onready var player = $Player
+@onready var button = $ElevatorArea/ProximityButton
+@onready var area = $ElevatorArea
+@export var trigger_distance: float = 100.0  # Change as needed
 
+func _process(_delta):
+	if player and area and button:
+		var distance = player.global_position.distance_to(button.global_position)
+		button.visible = distance <= trigger_distance
 
 	
--- three-knives-deep----daydream/Menu/typetext.gd
+++ three-knives-deep----daydream/Menu/typetext.gd
@@ -1,11 +1,15 @@
 extends Label
 
 
-@export var text_to_show: String = "You are the owner of a hotel. Suddenly, you are given a notice to shut down the hotel OR murder three specific people to keep your hotel open.  Solve the riddles given to you in each level to figure out who your targeted victim is. Keep the doors open. Kill three. The right ones. Let the hunt begin......"
+@export var text_to_show: String = "You are the owner of a hotel. Suddenly, you are given a notice to shut down the hotel OR murder three specific people to keep your hotel open.  Solve the riddles given to you in each level to figure out who your targeted victim is. Keep the doors open. Kill three. The right ones. 
+                                    Let the hunt begin......"
 @export var speed: float = 0.05 
+
+@onready var continue_button = get_parent().get_node("Button") 
 
 func _ready():
 	text = ""
+	continue_button.visible = false  
 	start_typing()
 	
 func start_typing():
@@ -14,4 +18,6 @@ func start_typing():
 		text += text_to_show[i]
 		i += 1
 		await get_tree().create_timer(speed).timeout
+	
+	continue_button.visible = true
 		
