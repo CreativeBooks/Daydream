e8bf3bcfb664b12ad685185a0e2875d54380acd5
PGarg111 <priyanshigarg111@icloud.com>
2025-10-10 17:17:37 -0400

    changed character with animated character

--- three-knives-deep----daydream/player.gd
+++ three-knives-deep----daydream/player.gd
@@ -9,7 +9,7 @@ var can_fall := true
 var just_respawned := false
 var jump_count = 0
 var respawn_position: Vector2
-
+@onready var animated_sprite = $AnimatedSprite2D
 const LIMIT_LEFT = 0
 const LIMIT_RIGHT = 4960
 
@@ -49,7 +49,14 @@ func _physics_process(delta: float) -> void:
 	else:
 		print("Player is still in the scene")
 
-	
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("walk")
+	else:
+		animated_sprite.play("jump")
+		
 
 func find_respawn_point():
 	var root = get_tree().current_scene
@@ -126,3 +133,5 @@ func _on_3_l_3_body_entered(body: Node2D) -> void:
 	if body == self:
 		print("🔄 Player entered reset zone — restarting level...")
 		get_tree().reload_current_scene()
+		
+		
